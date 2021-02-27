97c362a91223a02d380b27244a4e6aeeb743437f
Ahmed Farahat <dr_it@live.com>
2021-02-27 03:11:26 +0200

    Add project files.

---
+++ RSAEncryption/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Xml.Serialization;
+
+namespace RSAEncryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GenerateKeys();
+        }
+
+        #region-----Encryptionand Decryption Function-----
+        static public byte[] Encryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    encryptedData = RSA.Encrypt(Data, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+
+                return null;
+            }
+
+        }
+
+        static public byte[] Decryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] decryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    decryptedData = RSA.Decrypt(Data, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+
+                return null;
+            }
+
+        }
+        #endregion
+
+        #region--variables area
+        UnicodeEncoding ByteConverter = new UnicodeEncoding();
+        RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+        byte[] plaintext;
+        byte[] encryptedtext;
+        #endregion
+
+        #region-- Function Implemantation
+        private void button1_Click(object sender, EventArgs e)
+        {
+            plaintext = ByteConverter.GetBytes(txtplain.Text);
+            encryptedtext = Encryption(plaintext, _publicKey, false);
+            txtencrypt.Text = ByteConverter.GetString(encryptedtext);
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] decryptedtex = Decryption(encryptedtext, _privateKey, false);
+            txtdecrypt.Text = ByteConverter.GetString(decryptedtex);
+        }
+        #endregion
+
+        private RSAParameters _privateKey;
+        private RSAParameters _publicKey;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            GenerateKeys();
+        }
+
+        void GenerateKeys()
+        {
+            _privateKey = RSA.ExportParameters(true);
+            _publicKey = RSA.ExportParameters(false);
+
+            var sr = new StringWriter();
+            var xs = new XmlSerializer(typeof(RSAParameters));
+            xs.Serialize(sr, _privateKey);
+            textBox1.Text = sr.ToString();
+            sr = new StringWriter();
+            xs = new XmlSerializer(typeof(RSAParameters));
+            xs.Serialize(sr, _publicKey);
+            textBox2.Text = sr.ToString();
+
+        }
+    }
+}
